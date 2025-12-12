721905c644fedd83c88ac28162007d1cdebeaf29
Arseny <20096ninja@gmail.com>
2025-12-12 23:33:09 +0300

    Fri 12 Dec 23:33:09 MSK 2025

--- essential-packages.sh
+++ essential-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacstrap -K /mnt base base-devel linux linux-firmware fish git vim cryptsetup dhcpcd iwd openssh man sudo rsync
+pacstrap -K /mnt base base-devel linux linux-firmware fish git vim cryptsetup dhcpcd iwd openssh man sudo rsync \
   btrfs-progs \
   efibootmgr \
   limine \
